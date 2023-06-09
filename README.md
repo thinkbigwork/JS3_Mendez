c427ed2e6b52abe0b8aff917714d05f709e6a8c0
Martin <mailmartinmendez@gmail.com>
2023-06-08 22:57:41 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,62 @@
+working at home
+====================
+<img src="https://github.com/thinkbigwork/WorkingAtHome/blob/main/img/headers/slide_working_at_home.png" align="center" height="240" alt="GridDB"/>
+
+https://thinkbigwork.github.io/WorkingAtHome/index.html
+
+
+En tiempos de trabajo remoto, hacer un diagnóstico sobre las condiciones laborales dentro de nuestro propio hogar nos permite conocer cuáles son las problemáticas y las posibilidades de mejora de nuestro espacio de trabajo, y así poder trabajar de forma más eficiente, segura y sobre todo cuidar nuestra salud mental y física y cuidar el medio ambiente.
+
+Cómo funciona?
+-----------
+
+En escencia es un formulario tipo encuesta, pero lo que lo hace especial es su interactividad con el usuario para lograr una evaluación acorde a cada situación, ajustándose a cada necesidad, para evitar que el usuario pierda tiempo en preguntas standard que no tienen que ver con su situación particular.
+
+
+Estado de construcción
+-----------
+
+En esta primera etapa de JS la herramienta pretende que el usuario pueda evaluar su puesto de trabajo y todos sus elementos, a saber: espacio, escritorio, silla, ubicación de las pantallas, ubicación del teclado y mouse, las condiciones de ambiente como el nivel de iluminación, contrastes, reflejos, los riesgos ergonómicos, la calidad del aire, el ruido interno y externo, etc.
+
+
+
+¿Cómo empezar?
+-----------
+
+Es muy sencillo, carga el index.html. 
+
+
+
+
+¿Encuentras algún error?
+-----------
+
+Por favor, si encuentras algún bug no dejes de avisarnos, de esta manera la comunidad crece y eso nos beneficia a todos!
+
+
+
+Autores
+-------
+
+**Da Vinci**
+
++ http://twitter.com/davincielcapo
+
++ http://github.com/davincielcapo
+
+**Toretto**
+
++ http://twitter.com/torettomiautovuela
+
++ http://github.com/torettomiautovuela
+
+
+
+Copyright and license
+--------------------
+
+Copyright 2023 Toretto, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this work except in compliance with the License.
+You may obtain a copy of the License in the LICENSE file, or at:
---
+++ script.js
@@ -0,0 +1,48 @@
+//js para cuestionario
+let q1 = 0;
+let op = 0;
+let nombre = prompt('Introduce tu nombre');
+  nombre = nombre.toUpperCase();
+
+  function contenido(){
+    switch (op) {
+        case '1':
+            alert('====== PC DE ESCRITORIO ======= \nSi estás trabajando con una pc de escritorio debes tener en cuenta lo siguiente: \na - Medir de ruido de ventiladores del CPU en banda de octavas. \nb- Medir niveles de iluminación del ambiente, contraste y reflejos en el monitor. \nc- Medir niveles de carga térmica producto del sobrecalenamiento del CPU.')
+            break;
+        case '2':
+            alert('====== NOTEBOOK ======= \nSi estás trabajando con una notebook debes tener en cuenta lo siguiente: \na - Revisar tu posición postural si presentas dolor cervical. La inclinación de la pantalla puede ser una de las causas. \nb- La posición de las extremidades superiores puede verse comprometida por la altura donde está colocada la notebook. \nc- La luz de fondo de la pantalla, el contraste, la luminosidad, son detalles a los que debes prestar atención para evitar cansancio ocular y a la larga, pérdida de visión.')
+            break;
+        case '3':
+            alert('====== CELULAR ======= \nSi estás trabajando con un celular debes tener en cuenta lo siguiente: \na - El elevado uso de las extermidades superiores, especialmente de los dedos pulgares, puede provocar tendinitis y otros efectos en la salud. \nb- Trata de no estar trabajando en una habitación a oscuras por un tiempo prolongado, esto provocará daños a tu salud visual y neurológica. \nc- Estar constantemente revisando el celular en busca de nuevos mensajes de whatsapp y/o redes sociales puede provocar aumento de stress y en algunos casos episodios de pánico.');                   
+            break;
+        default: 
+            console.log('Opcion incorrecta'); //solo para visualizar que está sucediendo
+            break;
+    }
+  }
+
+  alert('Hola ' + nombre + ', comencemos con el cuestionario!');
+    do{
+        const question = '¿trabajas en forma remota?. Indica SI o NO';
+        q1 = prompt(question);
+        console.log(q1); //solo para visualizar que está sucediendo
+    }while(q1 !== 'SI' && q1 !== 'NO');
+    do{
+        if(q1=='SI'){
+                do{
+                    const seleccion = 'Ahora vamos a evaluar tus condiciones de trabajo. \nA continuación selecciona el equipo de trabajo que utilizás más frecuentemente: \n1 - PC de escritorio \n2 - notebook \n3 - celular' ;
+                    op = prompt(seleccion);
+                }while(op !== '1' && op !== '2' && op !=='3');
+                contenido();
+        }else{
+            alert('Entonces no tenemos ninguna recomendación para vos!');
+        }
+
+            do{
+                const continuar = 'Quieres volver a empezar ' + nombre + ' ?. Indica "SI" o "NO"';
+                q1 = prompt(continuar);
+            }while(q1 !== 'SI' && q1 !== 'NO');
+    }while(q1 == 'SI');
+
+
+  
