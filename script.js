9ae2e8438f919629e2befcc253b4f3d19b48933b
Martin <mailmartinmendez@gmail.com>
2023-06-29 00:01:43 -0300

    primer commit

--- script.js
+++ script.js
@@ -21,16 +21,18 @@ let nombre = prompt('Introduce tu nombre');
     }
   }
 
-  alert('Hola ' + nombre + ', comencemos con el cuestionario!');
+  alert('Hola ' + nombre + ', comencemos con la evaluación!');
     do{
-        const question = '¿trabajas en forma remota?. Indica SI o NO';
+        const question = '¿Tienes un lugar definido para trabajar en tu casa? Responde SI si trabajas en un solo lugar, sino responde NO';
         q1 = prompt(question);
+        q1 = q1.toUpperCase();
         console.log(q1); //solo para visualizar que está sucediendo
     }while(q1 !== 'SI' && q1 !== 'NO');
+
     do{
         if(q1=='SI'){
                 do{
-                    const seleccion = 'Ahora vamos a evaluar tus condiciones de trabajo. \nA continuación selecciona el equipo de trabajo que utilizás más frecuentemente: \n1 - PC de escritorio \n2 - Notebook \n3 - Celular' ;
+                    const seleccion = 
                     op = prompt(seleccion);
                 }while(op !== '1' && op !== '2' && op !=='3');
                 contenido();
@@ -41,8 +43,34 @@ let nombre = prompt('Introduce tu nombre');
             do{
                 const continuar = 'Quieres volver a empezar ' + nombre + ' ?. Indica "SI" o "NO"';
                 q1 = prompt(continuar);
+                q1 = q1.toUpperCase();
             }while(q1 !== 'SI' && q1 !== 'NO');
     }while(q1 == 'SI');
 
 
+    const seleccion = 'Ahora vamos a evaluar las condiciones de trabajo en esos lugares. \nA continuación selecciona el lugar de tu casa que más horas utilizás en el día para trabajar: \n1 - En un escritorio \n2 - En la mesa del comedor \n3 - En la cama' ;
+                  
   
+///Programa principal///
+//1- Function devices (carga): Array devices (nombre, marca, modelo, tipo)
+function userDevices (marca, modelo, tipo){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.tipo = tipo;
+}
+const userDevicesMobile = new userDevices ("Apple", "iphone", "celular");
+const userDevicesNotebook = new userDevices ("Asus", "Satellite", "notebook");
+const userDevicesDesktop = new userDevices ("Dell", "Miracle", "desktop");
+
+console.log (userDevicesMobile.marca);
+console.log (userDevicesNotebook.marca);
+console.log (userDevicesDesktop.marca); 
+
+function workPlaces (lugar, tiempo, compartido){
+    const workPlacesDesktop = new workPlaces ("Escritorio, 4, ")
+}
+
+//2- Function devices (lectura): lee el array y devuelve los nombres de los dispositivos
+
+//3- Function optionValidate(queValida, valoresAvalidar, valorValidado)
+//4- Function answerLoad: muestra una pregunta del array, y guarda una respuesta en la misma línea del array.
