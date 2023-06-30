f5f9c9f9b21a72333d9f4f9d96c4575eac666fd5
Martin <mailmartinmendez@gmail.com>
2023-06-29 23:59:05 -0300

    ultimos cambios sobre ppal

--- script.js
+++ script.js
@@ -137,18 +137,6 @@ function informe(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // programa principal
 usuario();
 remoto();
@@ -161,51 +149,3 @@ do{
 }while(q1 == 'SI');
 informe();
 // finaliza programa ppal.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*   function contenido(){
-    switch (op) {
-        case '1':
-            alert('====== PC DE ESCRITORIO ======= \nSi estás trabajando con una pc de escritorio debes tener en cuenta lo siguiente: \na - Medir de ruido de ventiladores del CPU en banda de octavas. \nb- Medir niveles de iluminación del ambiente, contraste y reflejos en el monitor. \nc- Medir niveles de carga térmica producto del sobrecalenamiento del CPU.')
-            break;
-        case '2':
-            alert('====== NOTEBOOK ======= \nSi estás trabajando con una notebook debes tener en cuenta lo siguiente: \na - Revisar tu posición postural si presentas dolor cervical. La inclinación de la pantalla puede ser una de las causas. \nb- La posición de las extremidades superiores puede verse comprometida por la altura donde está colocada la notebook. \nc- La luz de fondo de la pantalla, el contraste, la luminosidad, son detalles a los que debes prestar atención para evitar cansancio ocular y a la larga, pérdida de visión.')
-            break;
-        case '3':
-            alert('====== CELULAR ======= \nSi estás trabajando con un celular debes tener en cuenta lo siguiente: \na - El elevado uso de las extermidades superiores, especialmente de los dedos pulgares, puede provocar tendinitis y otros efectos en la salud. \nb- Trata de no estar trabajando en una habitación a oscuras por un tiempo prolongado, esto provocará daños a tu salud visual y neurológica. \nc- Estar constantemente revisando el celular en busca de nuevos mensajes de whatsapp y/o redes sociales puede provocar aumento de stress y en algunos casos episodios de pánico.');                   
-            break;
-        default: 
-            console.log('Opcion incorrecta'); //solo para visualizar que está sucediendo
-            break;
-    }
-  } */
-
-
-  
-
-                  
-function workPlaces (lugar, tiempo, compartido){
-    const workPlacesDesktop = new workPlaces ("Escritorio, 4, ")
-}
-
-//2- Function devices (lectura): lee el array y devuelve los nombres de los dispositivos
-
-//3- Function optionValidate(queValida, valoresAvalidar, valorValidado)
-//4- Function answerLoad: muestra una pregunta del array, y guarda una respuesta en la misma línea del array.
